2fef3fcfae44189dd1cc1c2364c55f43beb3a292
herba1 <burnedcard77@gmail.com>
2025-05-23 02:18:28 -0700

    mask button component

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,7 @@
 import { client } from "@/sanity/lib/client";
 import { inter } from "./fonts";
 import HeroSection from "./components/HeroSection";
+import MaskButton from "./ui/MaskButton";
 
 // If loading a variable font, you don't need to specify the font weight
 
@@ -47,7 +48,7 @@ export default async function Home() {
       {/* first section */}
       <div className="main__container ">
         <article className="main-content  bg-secondary flex justify-center items-center h-svh  ">
-          <h1 className="text-center text-4xl ">this is some content</h1>
+          here today
         </article>
         <article className="main-content  bg-secondary flex justify-center items-center h-svh  ">
           <h1 className="text-center text-4xl ">this is some content</h1>
---
+++ src/app/ui/MaskButton.jsx
@@ -0,0 +1,111 @@
+"use client";
+import { ArrowRight } from "lucide-react";
+import gsap from "gsap";
+import { useGSAP } from "@gsap/react";
+import { useRef } from "react";
+import Link from "next/link";
+
+export default function MaskButton({
+  text = "Gallery",
+  isLink = true,
+  href = "#",
+  padding = "12px",
+  gap = "4px",
+  className = "",
+}) {
+  const buttonContainer = useRef();
+  const tl = useRef();
+
+  const { contextSafe } = useGSAP(
+    () => {
+      tl.current = gsap.timeline({ paused: true });
+
+      gsap.set(".button__mask", {
+        clipPath: "inset(100% 0 0 0)",
+      });
+
+      tl.current
+        .to(
+          ".button__text",
+          {
+            xPercent: 100,
+            ease: "power3.inOut",
+            duration: 0.35,
+          },
+          "start"
+        )
+        .to(
+          ".button__mask",
+          {
+            clipPath: "inset(0% 0 0 0)",
+            ease: "power3.inOut",
+            duration: 0.35,
+          },
+          "start"
+        );
+    },
+    { scope: buttonContainer }
+  );
+
+  const start = contextSafe(() => {
+    tl.current.timeScale(1).play();
+  });
+  const reverse = contextSafe(() => {
+    setTimeout(() => {
+      tl.current.timeScale(1.2).reverse();
+    }, 50);
+  });
+
+  return (
+    <button
+      ref={buttonContainer}
+      className={` button__container transition-all active:scale-90 font-semibold relative w-fit h-fit bg-highlight text-secondary overflow-hidden rounded-lg ${className}`}
+      onMouseEnter={start}
+      onMouseLeave={reverse}
+    >
+      {isLink && (
+        <Link
+          href={href}
+          className="absolute top-0 left-0 right-0 w-full h-full bg-transparent text-transparent z-20"
+        >
+          {text}
+        </Link>
+      )}
+      <div className="relative w-full h-full">
+        <div
+          className={` button__text  flex items-center w-full h-full justify-center `}
+          style={{ padding:`${padding}` , gap:`${gap}`}}
+        >
+          <span>{text}</span>
+          <ArrowRight strokeWidth={2.5}></ArrowRight>
+        </div>
+        <div
+          className={` button__text  absolute top-0 -left-full w-full h-full  flex items-center justify-center `}
+          style={{ padding:`${padding}` , gap:`${gap}`}}
+        >
+          <span>{text}</span>
+          <ArrowRight strokeWidth={2.5}></ArrowRight>
+        </div>
+      </div>
+      {/* unmask */}
+      <div
+        className={` button__mask w-full h-full absolute z-10 left-0 top-0 text-primary bg-red-400`}
+          style={{ padding:`${padding}` , gap:`${gap}`}}
+      >
+        <div
+          className={` button__text  flex items-center w-full h-full justify-center  `}
+          style={{ padding:`${padding}` , gap:`${gap}`}}
+        >
+          <span>{text}</span>
+          <ArrowRight strokeWidth={2.5}></ArrowRight>
+        </div>
+        <div
+          className={` button__text  absolute top-0 -left-full w-full h-full  flex items-center justify-center `}
+        >
+          <span>{text}</span>
+          <ArrowRight strokeWidth={2.5}></ArrowRight>
+        </div>
+      </div>
+    </button>
+  );
+}
